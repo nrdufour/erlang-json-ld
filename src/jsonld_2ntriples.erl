883352f31637926aa9a0b0c54884827c6ee13e80
Nicolas R Dufour <nrdufour@gmail.com>
2011-01-07 14:52:42 -0500

    Finalize (almost) the 2ntriples utility.

--- src/jsonld_2ntriples.erl
+++ src/jsonld_2ntriples.erl
@@ -17,6 +17,9 @@
 
 -export([start/0]).
 
+-include("jsonld.hrl").
+
+-define(BNODE_PATTERN, "^_\\:\\w+$").
 
 start() ->
     case init:get_plain_arguments() of
@@ -27,9 +30,50 @@ start() ->
 parse_json_to_ntriples(JsonFile) ->
     Json = readlines(JsonFile),
     Triples = jsonld_proc:to_triples(Json),
-    io:format("JSON: ~p~n", [Json]),
-    io:format("Triples: ~p~n", [Triples]),
+    lists:foreach(
+        fun(Element) ->
+            NTriple = triple_to_ntriple(Element),
+            io:format("~s~n", [binary_to_list(NTriple)])
+        end,
+        Triples
+    ),
     ok.
+
+triple_to_ntriple(Triple) ->
+    TripleSubject = Triple#triple.subject,
+    TripleProperty = Triple#triple.property,
+    TripleObject = Triple#triple.object,
+    TripleType = Triple#triple.type,
+    Start = <<"<">>,
+    End = <<">">>,
+    Space = <<" ">>,
+    Quote = <<"\"">>,
+
+    %% subject
+    TripleSubject = Triple#triple.subject,
+    Subject = case re:run(TripleSubject, ?BNODE_PATTERN) of
+        {match, _} -> TripleSubject;
+        _ -> <<Start/binary, TripleSubject/binary, End/binary>>
+    end,
+
+    %% property
+    Property = <<Start/binary, TripleProperty/binary, End/binary>>,
+
+    %% object
+    Object = case re:run(TripleObject, ?BNODE_PATTERN) of
+        {match, _} -> TripleObject;
+        _ ->
+            case TripleType of
+                resource ->
+                    <<Start/binary, TripleObject/binary, End/binary>>;
+                literal ->
+                    %% TODO missing support for lang and datatype
+                    <<Quote/binary, TripleObject/binary, Quote/binary>>
+            end
+    end,
+
+    %% N triple
+    <<Subject/binary, Space/binary, Property/binary, Space/binary, Object/binary>>.
 
 readlines(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
