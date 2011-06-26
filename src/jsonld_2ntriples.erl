d8bf4523bcbf06854f2f3acc5e1e6dde83f4dc2d
Nicolas Dufour <nrdufour@gmail.com>
2011-06-26 15:11:20 -0400

    Change the license to MIT.

--- src/jsonld_2ntriples.erl
+++ src/jsonld_2ntriples.erl
@@ -1,16 +1,6 @@
-%% Copyright 2011 Nicolas R Dufour
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
-%% use this file except in compliance with the License. You may obtain a copy of
-%% the License at
-%%
-%%   http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-%% License for the specific language governing permissions and limitations under
-%% the License.
+
+% This file is part of erlang-json-ld released under the MIT license.
+% See the LICENSE file for more information.
 
 -module(jsonld_2ntriples).
 -author("Nicolas R Dufour <nrdufour@gmail.com>").
@@ -84,4 +74,3 @@ get_all_lines(Device, Accum) ->
         eof  -> file:close(Device), Accum;
         Line -> get_all_lines(Device, Accum ++ [Line])
     end.
-
--- src/jsonld_proc.erl
+++ src/jsonld_proc.erl
@@ -1,20 +1,10 @@
-%% Copyright 2011 Nicolas R Dufour
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
-%% use this file except in compliance with the License. You may obtain a copy of
-%% the License at
-%%
-%%   http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-%% License for the specific language governing permissions and limitations under
-%% the License.
 
-%%
-%% JSON-LD Processor based on Bradley Allen work
-%%
+% This file is part of erlang-json-ld released under the MIT license.
+% See the LICENSE file for more information.
+
+%
+% JSON-LD Processor based on Bradley Allen work
+%
 
 -module(jsonld_proc).
 -author("Nicolas R Dufour <nrdufour@gmail.com>").
@@ -59,7 +49,7 @@ process_local_context({struct, _Props} = JsonObject, InitialState) ->
     %% Local Context: merge if exists
     LocalContextProp = get_json_prop(<<"#">>, JsonObject),
     case LocalContextProp of
-        {<<"#">>, {struct, CtxProps}} -> 
+        {<<"#">>, {struct, CtxProps}} ->
             NewContext = merge_contexts(InitialState#state.context, CtxProps),
             InitialState#state{context = NewContext};
         false -> InitialState
@@ -180,7 +170,7 @@ process_resource(Object, ContextDict) ->
                 %% Curie
                 {_, {match, [Prefix, Reference]}, _, _} ->
                     case dict:is_key(Prefix, ContextDict) of
-                        true -> 
+                        true ->
                             PrefixNamespace = dict:fetch(Prefix, ContextDict),
                             <<PrefixNamespace/binary, Reference/binary>>;
                         false ->
